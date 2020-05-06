1025b950ad927964d78881097c3490b87f7d8947
Michael Bestas <mkbestas@lineageos.org>
2020-05-06 20:48:18 +0300

    Updater: Add privapp permissions xml
    Change-Id: If1953e76859850359a63dc4a4ab50940d76fd59f

--- Android.mk
+++ Android.mk
@@ -31,7 +31,17 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
+LOCAL_REQUIRED_MODULES := privapp_whitelist_com.aospextended.ota.xml
+
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := privapp_whitelist_com.aospextended.ota.xml
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := UpdatesStudio
